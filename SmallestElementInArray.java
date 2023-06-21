d1070ab44343b118cbafe6c860f2bf1266957918
shraddha0930 <shraddhaingole11@gmail.com>
2023-06-21 14:22:34 +0530

    Added:code to print the smallest element in an array

---
+++ SmallestElementInArray.java
@@ -0,0 +1,28 @@
+package day4assignment;
+
+import java.util.Scanner;
+
+public class SmallestElementInArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements : ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        System.out.println("Enter the elements");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        //find small number
+        int temp;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (array[i] > array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        System.out.println("smallest number:" + array[0]);
+    }
+}
